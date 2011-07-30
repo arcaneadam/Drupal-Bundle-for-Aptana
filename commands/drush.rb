b047cbb1e5833ba2104201ca64a3663fee7a80ee
Adam A. Gregory <adam@causecast.org>
2011-07-29 18:38:49 -0700

    Nightly Commit

--- commands/drush.rb
+++ commands/drush.rb
@@ -1,14 +1,19 @@
 require 'ruble'
 require 'drush_fn.rb'
 require 'multiple.rb'
+require 'java'
 
 Ruble::Logger.log_level = :trace
 
 with_defaults :input => :none, :output => :output_to_console, :working_directory => :current_project do |bundle|
   command 'Execute PHP' do |cmd|
+    cmd.key_binding = "CONTROL+D"
     cmd.output = ':discard'
     cmd.invoke do |context|
-      drush_init()
+      options = {}
+      options[:title] = "Enter PHP Code"
+      code = org.eclipse.jface.dialogs.InputDialog
+      CONSOLE.puts code
     end
   end
 end
